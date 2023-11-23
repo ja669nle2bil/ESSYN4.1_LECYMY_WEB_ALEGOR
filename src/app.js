eee1af193c50f727e39b3cdf8a3127b2127f0d47
ja669nle2bil <147039919+ja669nle2bil@users.noreply.github.com>
2023-11-23 09:50:54 +0100

    xd
    cxx

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
 console.log('Hello world!');
+
+function ishappynumber(n) {
+    // Initialize seen values set
+    const seen = new Set();
+
+    // Recursive helper function
+    function _isHappy(number) {
+        if (number === 1) {
+            return true;
+        }
+
+        if (seen.has(number)) {
+            return false;
+        }
+
+        seen.add(number);
+
+        // Calculate sum of squares of digits
+        const sqDigits = Math.floor(number % 10) ** 2 + Math.floor(number / 10) ** 2;
+
+        // Recursively check if sqDigits is happy
+        return _isHappy(sqDigits);
+    }
+
+    // Call the recursive function with the input number
+    return _isHappy(n);
+}
+console.log(ishappynumber(1)); // Output: true
+console.log(ishappynumber(44)); // Output: true
+console.log(ishappynumber(15)); // Output: false
